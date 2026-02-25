2c7ebe84fe0d9ef98faa3594895934b5b09fffc2
Geoffrey Martens <geoffreymartens@MacBook-Pro-van-Geoffrey.local>
2026-02-25 13:54:44 +0100

    updating startup script mac os

--- startup-script-macos-vms.sh
+++ startup-script-macos-vms.sh
@@ -27,7 +27,7 @@ fi
 # uninstall default unecessary packages from brew
 /opt/homebrew/bin/brew uinstall xcodes
 
-for package in xcodes mint xcodesorg/made/xcodes git-lfs coreutils azure-cli
+for package in xcodes mint xcodesorg/made/xcodes git-lfs coreutils
 do
     if ! brew list $package &> /dev/null; then
         echo "📦 Homebrew $package is not installed. Installing now..."
@@ -37,21 +37,19 @@ do
     fi
 done
 
-if ! -d /Users/admin/actions-runner/.runner; then
-    # recreate empty  action-runners directory
-    /bin/rm -rf /Users/admin/actions-runner && /bin/mkdir /Users/admin/actions-runner 
-    # download runner config
-    cd /Users/admin/actions-runner && curl -o actions-runner-osx-arm64-2.331.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.331.0/actions-runner-osx-arm64-2.331.0.tar.gz && /usr/bin/tar xzf ./actions-runner-osx-arm64-2.331.0.tar.gz
-    # retrieve runner token
-    GITHUBTOKEN = $(curl -k -X POST -H "Authorization: Bearer $GITHUB_PAT" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/belgianmobileid/actions/runners/registration-token | jq -r .token)
-    # run runner config
-    /Users/admin/actions-runner/config.sh --unattended --url https://github.com/belgianmobileid --token $GITHUBTOKEN --name $HOSTNAME --runnergroup $RUNNGERGROUP --labels $RUNNERLABELS
-    # install github runner as service and run
-    /Users/admin/actions-runner/svc.sh install && /Users/admin/actions-runner/svc.sh start
-fi
+# recreate empty  action-runners directory
+/bin/rm -rf /Users/admin/actions-runner && /bin/mkdir /Users/admin/actions-runner 
+# download runner config
+cd /Users/admin/actions-runner && curl -o actions-runner-osx-arm64-2.331.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.331.0/actions-runner-osx-arm64-2.331.0.tar.gz && /usr/bin/tar xzf ./actions-runner-osx-arm64-2.331.0.tar.gz
+# retrieve runner token
+GITHUBTOKEN=$(curl -k -X POST -H "Authorization: Bearer $GITHUB_PAT" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/belgianmobileid/actions/runners/registration-token | jq -r .token)
+# run runner config
+/Users/admin/actions-runner/config.sh --unattended --url https://github.com/belgianmobileid --token $GITHUBTOKEN --name $HOSTNAME --runnergroup $RUNNGERGROUP --labels $RUNNERLABELS --replace
+# install github runner as service and run
+/Users/admin/actions-runner/svc.sh install && /Users/admin/actions-runner/svc.sh start
 
-# change admin password
+# # change admin password
 /usr/bin/dscl . -passwd /Users/admin admin $ADMIN_PASSWORD
 
-# reboot vm
+# # reboot vm
 sudo /sbin/shutdown -r now 
